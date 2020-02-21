c10a7951d109f85bee6d1fc3044bf8ea642e9104
ruoyijiang <ruoyijiang@gmail.com>
2020-02-21 14:47:29 -0500

    column checking issue #44

--- scripts/light_cluster.py
+++ scripts/light_cluster.py
@@ -92,6 +92,13 @@ def lightCluster(heavy_file, light_file, out_file, doublets='drop', format='airr
     heavy_df = pd.read_csv(heavy_file, dtype='object', na_values=['', 'None', 'NA'], sep='\t')
     light_df = pd.read_csv(light_file, dtype='object', na_values=['', 'None', 'NA'], sep='\t')
 
+    # column checking
+    expected_columns = [cell_id, clone_id, v_call, j_call, junction_length, umi_count]
+    if set(expected_columns).issubset(heavy_df.columns) is False:
+        raise ValueError("Missing one or more columns in heavy chain file: " + ", ".join(expected_columns))
+    if set(expected_columns).issubset(light_df.columns) is False:
+        raise ValueError("Missing one or more columns in light chain file: " + ", ".join(expected_columns))
+
     # Fix types
     heavy_df[junction_length] = heavy_df[junction_length].astype('int')
     light_df[junction_length] = light_df[junction_length].astype('int')
