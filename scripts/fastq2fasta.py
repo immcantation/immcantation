205a1bbdc69011a07a1c3cc5fa06273ce452195f
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2020-11-18 17:22:06 -0800

    Update fastq2fasta for compatibility with biopython v1.71.

--- scripts/fastq2fasta.py
+++ scripts/fastq2fasta.py
@@ -2,6 +2,8 @@
 """
 Converts FASTQ to FASTA
 """
+import Bio
+from pkg_resources import parse_version
 from os import path
 from sys import argv
 from Bio import SeqIO
@@ -12,7 +14,16 @@ out_file = '%s.fasta' % path.splitext(out_file)[0]
 
 with open(out_file, 'w') as out_handle:
     records = SeqIO.parse(in_file, 'fastq')
-    writer = SeqIO.FastaIO.FastaWriter(out_handle, wrap=None)
-    writer.write_file(records)
+    if parse_version(Bio.__version__) >= parse_version('1.71'):
+        # Biopython >= v1.71
+        SeqIO.write(records, out_handle, format='fasta-2line')
+    else:
+        # Biopython < v1.71
+        writer = SeqIO.FastaIO.FastaWriter(out_handle, wrap=None)
+        writer.write_file(records)
 
 print(out_file)
+
+
+
+
