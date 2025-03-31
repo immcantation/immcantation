6c5ad0b633356233d490a357ea187ff636606464
Susanna Marquez <susanna.marquez@yale.edu>
2025-03-31 22:36:24 +0200

    added announcement

---
+++ docs/_static/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    // Function to load announcements and insert them into the DOM
+    function loadAnnouncements(url, targetSelector) {
+        $.get(url, function (data) {
+            // Create a new div and add the fetched content
+            const newDiv = $("<div>").html(data);
+
+            // Insert the new div after the target element
+            $(targetSelector).after(newDiv);
+        }).fail(function () {
+            console.error("Failed to load the message from the external file: " + url);
+        });
+    }
+
+    // Load Immcantation announcements and insert into a new div
+    loadAnnouncements("https://raw.githubusercontent.com/immcantation/immcantation/refs/heads/master/announcements.html", "#welcome-to-the-immcantation-portal > h1");
+
+});
